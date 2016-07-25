bb978a8668a3ed11756997ea8a978396ced809d5
Ivan Medvedev <medvedev.is@outlook.com>
2016-07-25 19:01:29 +0700

    Init Strings::numEnding()

--- Strings.php
+++ Strings.php
@@ -18,8 +18,7 @@ class Strings {
 	}
 
 	/**
-	 * Examples: "0,32"->32; "-1.32"->-132; "1.212"->throw Exception
-	 *
+	 * @param string $amount "0,32"->32; "-1.32"->-132; "1.212"->throw Exception
 	 * @throws Exceptions\MoneyInvalid
 	 */
 	static function toMoneyKop (string $amount) : int {
@@ -27,5 +26,29 @@ class Strings {
 		if (!preg_match('/^(-)?([0-9]+)(\.([0-9]{0,2}))?$/', $amount))
 			throw new Exceptions\MoneyInvalid;
 		return (int)($amount * 100);
+	}
+
+	/** @param string[] $endingArray array('яблоко', 'яблока', 'яблок') */
+	static private function numEnding (int $number, array $strings) : string {
+		$numberPr = $number % 100;
+		if ($numberPr >= 11 && $numberPr <= 19)
+			$res = $strings[2];
+		else {
+			$i = $numberPr % 10;
+			switch ($i) {
+				case 1:
+					$res = $strings[0];
+					break;
+				case 2:
+				case 3:
+				case 4:
+					$res = $strings[1];
+					break;
+				default:
+					$res = $strings[2];
+
+			}
+		}
+		return $res;
 	}
 }
